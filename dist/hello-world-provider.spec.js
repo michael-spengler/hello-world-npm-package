5cc1831c7095772e8fc1db8dc4577a66a352bf04
Michael Spengler <michael.spengler@sap.com>
2019-01-26 22:08:22 +0100

    improved readability - tests as documentation

--- dist/hello-world-provider.spec.js
+++ dist/hello-world-provider.spec.js
@@ -1,12 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const hello_world_provider_1 = require("./hello-world-provider");
-let helloWorldProvider;
 describe("HelloWorldProvider", () => {
-    beforeEach(async () => {
-        helloWorldProvider = new hello_world_provider_1.HelloWorldProvider();
-    });
     it("should provide hello world", async () => {
+        const helloWorldProvider = new hello_world_provider_1.HelloWorldProvider();
         expect(helloWorldProvider.provideHelloWorld())
             .toEqual("Hello World");
     });
--- src/hello-world-provider.spec.ts
+++ src/hello-world-provider.spec.ts
@@ -1,13 +1,11 @@
 import { HelloWorldProvider } from "./hello-world-provider"
 
-let helloWorldProvider: HelloWorldProvider
-
 describe("HelloWorldProvider", () => {
-    beforeEach(async () => {
-        helloWorldProvider = new HelloWorldProvider()
-    })
 
     it("should provide hello world", async () => {
+        const helloWorldProvider: HelloWorldProvider =
+            new HelloWorldProvider()
+
         expect(helloWorldProvider.provideHelloWorld())
             .toEqual("Hello World")
     })
